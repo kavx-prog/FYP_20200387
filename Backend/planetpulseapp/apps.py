136dcc8a3adc48cad6d96c63724443a6c35caf45
oshidhie <oshidhie@gmail.com>
2024-04-03 08:20:42 +0530

    backend changes

--- Backend/planetpulseapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-# from .scheduling import schedule_tasks
-
-class PlanetpulseappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'planetpulseapp'
